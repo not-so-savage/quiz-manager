111107c62c9f5c6f52ffb392e39dc112b1aefac0
Jack Savage <jacksvg95@gmail.com>
2021-08-17 14:48:50 +0100

    initial commit

---
+++ src/test/java/com/bjss/jacksavage/quizmanager/QuizManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjss.jacksavage.quizmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
